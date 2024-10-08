88aa7580e4035069e35fdb58121d96d190291133
Leo-Brd <leobordet.pro@gmail.com>
2024-10-08 15:51:38 +0200

    add mongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://LeoBrd:43LHxFbLd4X2f7oy@openc-gofullstack.oltcf.mongodb.net/?retryWrites=true&w=majority&appName=OpenC-GoFullstack',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 const app = express();
 
