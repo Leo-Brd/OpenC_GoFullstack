e9b1b09c3b48a7e623a57bee634e06276d9fe503
Leo-Brd <leobordet.pro@gmail.com>
2024-10-08 12:18:30 +0200

    add first middlewares and server config

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, use, next) => {
+    console.log("test");
+    next();
+})
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) => {
+    res.json({ message : 'Votre requete a bien ete recue'})
+})
+
 module.exports = app;
